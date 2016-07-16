355112603041ce12e4048c5f429a099f1103d698
mfxtreme <moefaridi@gmail.com>
2016-07-15 23:02:49 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', function (socket) {
+	socket.on('join_room',function(room){
+		socket.join(room);
+		socket.in(room).emit('joined_room', 'true');
+	});
+
+    socket.on('blob', function (room) {
+		socket.in(room.name).emit('some_typing', {cur_loc:room.cursor_location});
+    });
+	socket.on('disconnect', function(){
+		if (io.sockets.connected[socket.id]) {
+			io.sockets.connected[socket.id].disconnect();
+		}
+	});
+});
