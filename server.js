52a59b0a8198fa1bfa65f66ff5eb59afff037c75
mfxtreme <moefaridi@gmail.com>
2016-07-27 01:01:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on('connection', function (socket) {
 	});
 	
 	socket.on('editing', function (room) {
-		var count = io.sockets.adapter.rooms[room];
+		var count = io.sockets.adapter.rooms[room.name];
 		if (count.length >= 1) {
 			socket.in(room.name).emit('update', {text:room.text,cursor_location:room.cursor_location});
 		}
